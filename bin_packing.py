03094769d023ea93d6ccafc92cb66a84caee61c8
Michael Shihrer <michael.shihrer@gmail.com>
2016-03-08 00:28:13 -0700

    Pythonized code

--- bin_packing.py
+++ bin_packing.py
@@ -1,32 +1,34 @@
 import operator
 from collections import deque
 import itertools
+
+
 class Packer:
     def __init__(self):
         self.Tree = None
 
 
 def find_solution(rectangles, throttle):
-    sortedRectangles = []
+    sorted_rectangles = []
     
     # Add original index location to rectangles - necessary for putting tuples back in order for results
     # Probably a more pythonic way to do this...
     for i, rectangle in enumerate(rectangles):
         new_tuple = rectangle + (i,)
-        sortedRectangles.append(new_tuple)
+        sorted_rectangles.append(new_tuple)
 
     # Sort rectangles by height then width.
     # Going for a decreasing height, decreasing width best fit type of solution.
-    sortedRectangles.sort(key=operator.itemgetter(1, 0), reverse=True)
+    sorted_rectangles.sort(key=operator.itemgetter(1, 0), reverse=True)
     results = []
 
     # Create tree
     packed_tree = Tree(throttle)
 
     # Place sorted rectangles
-    for rectangle in sortedRectangles:
+    for rectangle in sorted_rectangles:
         result = packed_tree.add(rectangle)
-        results.append(result.rectTuple + result.coordinates)
+        results.append(result.rect_tuple + result.coordinates)
 
     # Return results to original order
     results.sort(key=operator.itemgetter(2))
@@ -41,6 +43,8 @@ def find_solution(rectangles, throttle):
 
 empty_spaces = deque()
 # The "tree" is more of a dynamic solution for placing rectangles
+
+
 class Tree:
     def __init__(self, throttle):
         self.root = None
@@ -51,16 +55,16 @@ class Tree:
         if self.root is None:                                   # Check to see if we have initialized root node
             self.root = Node(rectangle, (0, 0))                 # Root node originates at(0,0).
             empty_spaces.append(self.root)
-            self.root.splitSpace(rectangle)                     # Create space for next root.
-            currentNode = self.root                             # Place answer in this root.
+            self.root.split_space(rectangle)                     # Create space for next root.
+            current_node = self.root                             # Place answer in this root.
         else:
-            currentNode = self.search_spaces(rectangle)          # Find space to fit.
-            if currentNode is not None:                         # Check to see if space was found.
-                currentNode.splitSpace(rectangle)               # Create child nodes.
+            current_node = self.search_spaces(rectangle)          # Find space to fit.
+            if current_node is not None:                         # Check to see if space was found.
+                current_node.splitSpace(rectangle)               # Create child nodes.
             else:
-                currentNode = self.growTree(rectangle)          # No space found.  Add more.
+                current_node = self.grow_tree(rectangle)          # No space found.  Add more.
 
-        return currentNode                                      # Return answer.
+        return current_node                                      # Return answer.
 
     # Best-fit iterative search.
     def search_spaces(self, rectangle):
@@ -100,13 +104,13 @@ class Tree:
         return best_fit
 
     # Determine which way to grow in order to add space.
-    def growTree(self, rectangle):
-        goDown = rectangle[0] <= self.root.rectTuple[0]
-        goRight = rectangle[1] <= self.root.rectTuple[1]
+    def grow_tree(self, rectangle):
+        go_down = rectangle[0] <= self.root.rectTuple[0]
+        go_right = rectangle[1] <= self.root.rectTuple[1]
 
         # Sometimes it's not bad to be square.
-        defGoDown = goDown and (self.root.rectTuple[0] >= (self.root.rectTuple[1] + rectangle[1]))
-        defGoRight = goRight and (self.root.rectTuple[1] >= (self.root.rectTuple[0] + rectangle[0]))
+        def_go_down = go_down and (self.root.rectTuple[0] >= (self.root.rectTuple[1] + rectangle[1]))
+        def_go_right = go_right and (self.root.rectTuple[1] >= (self.root.rectTuple[0] + rectangle[0]))
 
         # if self.root.rectTuple[0] + rectangle[0] > self.root.rectTuple[1] + rectangle[1]:
         #     defGoDown = True
@@ -114,14 +118,14 @@ class Tree:
         #     defGoRight = True
 
         # These checks attempt to keep the working area square.
-        if defGoRight:
-            return self.growTreeRight(rectangle)
-        elif defGoDown:
-            return self.growTreeDown(rectangle)
-        elif goRight:
-            return self.growTreeRight(rectangle)
-        elif goDown:
-            return self.growTreeDown(rectangle)
+        if def_go_right:
+            return self.grow_tree_right(rectangle)
+        elif def_go_down:
+            return self.grow_tree_down(rectangle)
+        elif go_right:
+            return self.grow_tree_right(rectangle)
+        elif go_down:
+            return self.grow_tree_down(rectangle)
         else:
             return None # this is bad.  Avoid this!
 
@@ -129,21 +133,21 @@ class Tree:
     # Swap in new root.
     # Put old root in proper child.
     # I could probably generalize this...out of time
-    def growTreeRight(self, rectangle):
-        newRootDimensions = (self.root.rectTuple[0] + rectangle[0], self.root.rectTuple[1])
-        newRoot = Node(newRootDimensions, (0, 0))
-        newRoot.isEmpty = False
-        newRoot.leftChild = self.root
+    def grow_tree_right(self, rectangle):
+        new_root_dimensions = (self.root.rect_tuple[0] + rectangle[0], self.root.rect_tuple[1])
+        new_root = Node(new_root_dimensions, (0, 0))
+        new_root.is_empty = False
+        new_root.left_child = self.root
 
-        newRightChildSize = (rectangle[0], self.root.rectTuple[1])
-        newRightChildCoords = (self.root.rectTuple[0], 0)
-        newRoot.rightChild = Node(newRightChildSize, newRightChildCoords)
+        new_right_childsize = (rectangle[0], self.root.rect_tuple[1])
+        new_right_childcoords = (self.root.rect_tuple[0], 0)
+        new_root.right_child = Node(new_right_childsize, new_right_childcoords)
 
-        self.root = newRoot
+        self.root = new_root
 
         # Right child is new. Add it to spaces only if it can fit something.
-        if self.root.rightChild.rectTuple[0] > 0 and self.root.rightChild.rectTuple[1] > 0:
-            empty_spaces.appendleft(newRoot.rightChild)
+        if self.root.right_child.rectTuple[0] > 0 and self.root.right_child.rectTuple[1] > 0:
+            empty_spaces.appendleft(new_root.right_child)
 
         # some_node = self.search_spaces(rectangle)
         # if some_node:
@@ -151,25 +155,25 @@ class Tree:
         #     return some_node
         # else:
         #     return None
-        self.root.rightChild.splitSpace(rectangle)
-        return self.root.rightChild
+        self.root.right_child.split_space(rectangle)
+        return self.root.right_child
 
-    def growTreeDown(self, rectangle):
-        newRootDimensions = (self.root.rectTuple[0], self.root.rectTuple[1] + rectangle[1])
-        newRoot = Node(newRootDimensions, (0, 0))
-        newRoot.isEmpty = False
-        newRoot.rightChild = self.root
+    def grow_tree_down(self, rectangle):
+        new_root_dimensions = (self.root.rect_tuple[0], self.root.rect_tuple[1] + rectangle[1])
+        new_root = Node(new_root_dimensions, (0, 0))
+        new_root.is_empty = False
+        new_root.right_child = self.root
 
-        newLeftChildSize = (self.root.rectTuple[0], rectangle[1])
-        newLeftChildCoords = (0, self.root.rectTuple[1], rectangle[2])
-        newRoot.leftChild = Node(newLeftChildSize, newLeftChildCoords)
+        new_left_childize = (self.root.rect_tuple[0], rectangle[1])
+        new_left_childcoords = (0, self.root.rect_tuple[1], rectangle[2])
+        new_root.left_child = Node(new_left_childize, new_left_childcoords)
 
         # Replace old root
-        self.root = newRoot
+        self.root = new_root
 
         # Left child is new.  Add it to spaces only if it can fit something.
-        if self.root.leftChild.rectTuple[0] > 0 and self.root.leftChild.rectTuple[1] > 0:
-            empty_spaces.appendleft(self.root.leftChild)
+        if self.root.left_child.rectTuple[0] > 0 and self.root.left_child.rectTuple[1] > 0:
+            empty_spaces.appendleft(self.root.left_child)
 
         # some_node = self.search_spaces(rectangle)
         # if some_node:
@@ -177,52 +181,52 @@ class Tree:
         #     return some_node
         # else:
         #     return None
-        self.root.leftChild.splitSpace(rectangle)
-        return self.root.leftChild
+        self.root.left_child.split_space(rectangle)
+        return self.root.left_child
 
 
 # Node represents "space"
 # Can be empty or not empty.
 class Node:
     def __init__(self, rectangle, coords):
-        self.leftChild = None
-        self.rightChild = None
+        self.left_child = None
+        self.right_child = None
 
         # Stores the width/height of node
-        self.rectTuple = rectangle
+        self.rect_tuple = rectangle
         # Stores the upper left coordinates of node
         self.coordinates = coords
         # Just states that there is something placed in the current node.
-        self.isEmpty = True
+        self.is_empty = True
 
     # This splits the node and adds new child nodes
-    def splitSpace(self, rect):
+    def split_space(self, rect):
         # Remove space since it's no longer empty.  Might not really help since it takes time to delete.
         empty_spaces.remove(self)
-        self.isEmpty = False
+        self.is_empty = False
 
         # Sizes for new children
-        newLeftDimen = (self.rectTuple[0], self.rectTuple[1] - rect[1])
-        newRightDimen = (self.rectTuple[0] - rect[0], rect[1])
+        new_left_dimen = (self.rect_tuple[0], self.rect_tuple[1] - rect[1])
+        new_right_dimen = (self.rect_tuple[0] - rect[0], rect[1])
 
         # Starting coordinates for new children
-        newLeftCoords = (self.coordinates[0], self.coordinates[1] + rect[1])
-        newRightCoords = (self.coordinates[0] + rect[0], self.coordinates[1])
+        new_left_coords = (self.coordinates[0], self.coordinates[1] + rect[1])
+        new_right_coords = (self.coordinates[0] + rect[0], self.coordinates[1])
 
         # create child nodes
-        self.leftChild = Node(newLeftDimen, newLeftCoords)
-        self.rightChild = Node(newRightDimen, newRightCoords)
+        self.left_child = Node(new_left_dimen, new_left_coords)
+        self.right_child = Node(new_right_dimen, new_right_coords)
 
         # Add these nodes to our spaces list.
         # Only if they could possibly fit something in the future.
         # Major optimization since sometimes child nodes will have a dimension of zero
-        if self.leftChild.rectTuple[0] > 0 and self.leftChild.rectTuple[1] > 0:
-            empty_spaces.appendleft(self.leftChild)
+        if self.left_child.rect_tuple[0] > 0 and self.left_child.rect_tuple[1] > 0:
+            empty_spaces.appendleft(self.left_child)
         else:
-            self.leftChild.isEmpty = False
-        if self.rightChild.rectTuple[0] > 0 and self.rightChild.rectTuple[0] > 0:
-            empty_spaces.appendleft(self.rightChild)
+            self.left_child.is_empty = False
+        if self.right_child.rect_tuple[0] > 0 and self.right_child.rect_tuple[0] > 0:
+            empty_spaces.appendleft(self.right_child)
         else:
-            self.rightChild.isEmpty = False
+            self.right_child.is_empty = False
         # Change current node's size
-        self.rectTuple = rect
+        self.rect_tuple = rect
